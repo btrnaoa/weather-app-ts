f2b057607a4741ce4c067554ce924f425e57d3dc
Bao Tran <btrnaoa@gmail.com>
2021-01-09 14:38:04 +1100

    Add location form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 export default function App() {
-  return <></>;
+  const [location, setLocation] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLocation('');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <form className="location-form" onSubmit={handleSubmit}>
+        <label htmlFor="location">
+          Location
+          <input
+            className="text-input"
+            type="text"
+            id="location"
+            placeholder="e.g. Melbourne, AU"
+            value={location}
+            onChange={handleChange}
+          />
+        </label>
+        <input className="btn" type="submit" value="Go" />
+      </form>
+    </div>
+  );
 }
