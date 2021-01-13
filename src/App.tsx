825f4f1a04c4c1a9ddb2a6da8ad0d0c35eb1ed11
Bao Tran <btrnaoa@gmail.com>
2021-01-13 15:42:33 +1100

    Add fetch API to load weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './css/weather-icons.css';
 import WeatherCard from './components/WeatherCard';
 
+interface Forecast {
+  timezone_offset: number;
+  daily: Daily[];
+}
+
+interface Daily {
+  dt: number;
+  temp: {
+    min: number;
+    max: number;
+  };
+  weather: Weather[];
+}
+
+interface Weather {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  id: number;
+}
+
+const useFetch = (query: string) => {
+  const [forecast, setForecast] = useState<Forecast | null>(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response1 = await fetch(
+          `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.REACT_APP_API_KEY}`,
+        );
+        const weather: Weather = await response1.json();
+        const { lon, lat } = weather.coord;
+
+        // One Call API only accepts lat long coords so I used OWM's other API to get the coords by city name instead of signing up for another API key just for geocoding 😬
+        const response2 = await fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&exclude=current,minutely,hourly,alerts&units=metric`,
+        );
+        const json = await response2.json();
+        setForecast(json);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query]);
+  return { forecast, error, isLoading };
+};
+
 export default function App() {
   const [location, setLocation] = useState('');
+  const [query, setQuery] = useState('');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setQuery(location);
     setLocation('');
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLocation(e.target.value);
   };
+
+  const weather = useFetch(query);
 
   return (
     <div className="App">
@@ -32,11 +90,20 @@ export default function App() {
         <input className="btn" type="submit" value="Go" />
       </form>
       <div className="container">
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
+        {weather.forecast?.daily.map((item) => {
+          const time = item.dt + weather.forecast!.timezone_offset;
+          const { min, max } = item.temp;
+          const weatherId = item.weather[0].id;
+          return (
+            <WeatherCard
+              key={time}
+              time={time}
+              min={min}
+              max={max}
+              weatherId={weatherId}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function WeatherCard() {
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+interface WeatherCardProps {
+  time: number;
+  min: number;
+  max: number;
+  weatherId: number;
+}
+
+export default function WeatherCard({
+  time,
+  min,
+  max,
+  weatherId,
+}: WeatherCardProps) {
+  const idx = new Date(time * 1000).getUTCDay();
   return (
     <div className="weather-card">
-      <span className="muted">Mon</span>
-      <i className="weather-icon wi wi-owm-200" />
+      <span className="muted">{DAYS[idx]}</span>
+      <i className={`weather-icon wi wi-owm-${weatherId}`} />
       <div className="temps">
-        <span>24&deg;</span>
-        <span className="muted">12&deg;</span>
+        <span>{Math.round(max)}&deg;</span>
+        <span className="muted">{Math.round(min)}&deg;</span>
       </div>
     </div>
   );
 }
+
+WeatherCard.propTypes = {
+  time: PropTypes.number.isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  weatherId: PropTypes.number.isRequired,
+};
