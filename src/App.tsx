5e64a15e9f1415e8cd93a84eba65ea9c44f4f9b9
Bao Tran <btrnaoa@gmail.com>
2021-01-13 20:48:42 +1100

    Add error handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import './css/weather-icons.css';
 import WeatherCard from './components/WeatherCard';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 interface Forecast {
   timezone_offset: number;
   daily: Daily[];
@@ -18,6 +20,8 @@ interface Daily {
 }
 
 interface Weather {
+  cod: number;
+  message: string;
   coord: {
     lon: number;
     lat: number;
@@ -33,27 +37,34 @@ const useFetch = (query: string) => {
   const [city, setCity] = useState('');
   const [country, setCountry] = useState('');
   const [forecast, setForecast] = useState<Forecast | null>(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<Error | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setError(null);
     const fetchData = async () => {
       try {
         const response1 = await fetch(
-          `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.REACT_APP_API_KEY}`,
+          `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`,
         );
         const weather: Weather = await response1.json();
+
+        const { cod, message } = weather;
+        if (cod !== 200 && message) {
+          throw new Error(message);
+        }
+
         const { lon, lat } = weather.coord;
         setCity(weather.name);
         setCountry(weather.sys.country);
 
         // One Call API only accepts lat long coords so I used OWM's other API to get the coords by city name instead of signing up for another API key just for geocoding 😬
         const response2 = await fetch(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&exclude=current,minutely,hourly,alerts&units=metric`,
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&exclude=current,minutely,hourly,alerts&units=metric`,
         );
         const json = await response2.json();
         setForecast(json);
-      } catch (err) {
-        setError(err);
+      } catch (e) {
+        setError(e);
       } finally {
         setIsLoading(false);
       }
@@ -97,8 +108,10 @@ export default function App() {
         </label>
         <input className="btn" type="submit" value="Go" />
       </form>
-      <div className="location">
-        {!weather.isLoading && `${weather.city}, ${weather.country}`}
+      <div className="status">
+        {!weather.error
+          ? !weather.isLoading && `${weather.city}, ${weather.country}`
+          : weather.error.message}
       </div>
       <div className="container">
         {weather.forecast?.daily.map((item) => {
