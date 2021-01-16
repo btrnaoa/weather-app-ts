a0bba98e65ed3cebee5341e7407dfd0c7d218eb0
Bao Tran <btrnaoa@gmail.com>
2021-01-16 18:06:16 +1100

    Refactor interfaces and useFetch state

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import WeatherCard from './components/WeatherCard';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 interface Forecast {
-  timezone_offset: number;
+  city: string;
+  country: string;
   daily: Daily[];
+  timezone_offset: number;
 }
 
 interface Daily {
@@ -20,22 +22,24 @@ interface Daily {
 }
 
 interface Weather {
+  id: number;
+}
+
+interface CurrentWeatherData {
   cod: number;
-  message: string;
   coord: {
     lon: number;
     lat: number;
   };
+  id: number;
+  message: string;
+  name: string;
   sys: {
     country: string;
   };
-  name: string;
-  id: number;
 }
 
 const useFetch = (query: string) => {
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
   const [forecast, setForecast] = useState<Forecast | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -43,26 +47,25 @@ const useFetch = (query: string) => {
     setError(null);
     const fetchData = async () => {
       try {
-        const response1 = await fetch(
+        const res1 = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`,
         );
-        const weather: Weather = await response1.json();
+        const data: CurrentWeatherData = await res1.json();
+        const { cod, message, coord, sys, name } = data;
 
-        const { cod, message } = weather;
         if (cod !== 200 && message) {
           throw new Error(message);
         }
 
-        const { lon, lat } = weather.coord;
-        setCity(weather.name);
-        setCountry(weather.sys.country);
-
-        // One Call API only accepts lat long coords so I used OWM's other API to get the coords by city name instead of signing up for another API key just for geocoding 😬
-        const response2 = await fetch(
+        const { lon, lat } = coord;
+        const res2 = await fetch(
           `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&exclude=current,minutely,hourly,alerts&units=metric`,
         );
-        const json = await response2.json();
-        setForecast(json);
+        setForecast({
+          ...(await res2.json()),
+          city: name,
+          country: sys.country,
+        });
       } catch (e) {
         setError(e);
       } finally {
@@ -73,7 +76,7 @@ const useFetch = (query: string) => {
       fetchData();
     }
   }, [query]);
-  return { city, country, forecast, error, isLoading };
+  return { forecast, error, isLoading };
 };
 
 export default function App() {
@@ -90,7 +93,7 @@ export default function App() {
     setLocation(e.target.value);
   };
 
-  const weather = useFetch(query);
+  const weatherData = useFetch(query);
 
   return (
     <div className="App">
@@ -109,13 +112,14 @@ export default function App() {
         <input className="btn" type="submit" value="Go" />
       </form>
       <div className="status">
-        {!weather.error
-          ? !weather.isLoading && `${weather.city}, ${weather.country}`
-          : weather.error.message}
+        {!weatherData.error
+          ? !weatherData.isLoading &&
+            `${weatherData.forecast?.city}, ${weatherData.forecast?.country}`
+          : weatherData.error.message}
       </div>
       <div className="container">
-        {weather.forecast?.daily.map((item) => {
-          const time = item.dt + weather.forecast!.timezone_offset;
+        {weatherData.forecast?.daily.map((item) => {
+          const time = item.dt + weatherData.forecast!.timezone_offset;
           const { min, max } = item.temp;
           const weatherId = item.weather[0].id;
           return (
